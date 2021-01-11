fc499dc60ee3f08dd5555a9b7f95a011cea938b3
julianegtrece <juliannegtrece@gmail.com>
2021-01-11 18:43:24 -0300

    Primera version de tienda online flicky

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 var elem = document.querySelector('.container-slides');
-var flkty = new Flickity( elem, {
+var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
   contain: true
@@ -7,9 +7,9 @@ var flkty = new Flickity( elem, {
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.container-slides', {
-    cellAlign: 'left',
-    contain: true
+var flkty = new Flickity('.container-slides', {
+  cellAlign: 'left',
+  contain: true
 });
 
 
@@ -21,7 +21,7 @@ var flkty = new Flickity( '.container-slides', {
 // selectores de carrito
 
 const listaProductos = document.querySelector('.producto');
-const contenedorCarrito =  document.querySelector(".box-card-items");
+const contenedorCarrito = document.querySelector(".box-card-items");
 
 
 // listeners
@@ -37,73 +37,147 @@ let articulosCarrito = [];
 
 
 
-$('.botonCompra').on( 'click', function(event, pointer, cellElement, cellIndex) {
-  console.log(event.currentTarget.parentElement.parentElement); 
+$('.botonCompra').on('click', function (event, pointer, cellElement, cellIndex) {
+  // console.log(event.currentTarget.parentElement.parentElement);
   //accedo al elemento padre del evento
   const productoSeleccionado = event.currentTarget.parentElement.parentElement;
 
   obtenerDatos(productoSeleccionado);
-
 });
 
-function obtenerDatos(producto){
+
+$('.boton-vaciar').on('click', function (event, pointer, cellElement, cellIndex) {
+  // console.log(event.currentTarget.parentElement.parentElement);
+  //accedo al elemento padre del evento
+  vaciarCarrito();
+});
+
+
+
+
+
+
+
+
+window.onload=function () {
+	articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+	insertarCarritoHTML();
+}
+
+
+
+
+
+function obtenerDatos(producto) {
   const productoObjeto = {
     imagen: producto.querySelector('img').src,
-		nombre: producto.querySelector('h1').textContent,
-		precio: producto.querySelector('.price').textContent,
-		cantidad: 1
-  
-  
+    nombre: producto.querySelector('h1').textContent,
+    precio: producto.querySelector('.price').textContent,
+		id: producto.querySelector('button').getAttribute('data-id'),
+    cantidad: 1
+
+
   }
 
-  console.log(productoObjeto);
+  const existe = articulosCarrito.some(producto => producto.id == productoObjeto.id);
+
+  
+  if (existe){
+    const productos = articulosCarrito.map(producto=>{
+      if(producto.id == productoObjeto.id){
+        producto.cantidad++;
+        return producto;
+      }else{
+        return producto;
+      }
+      
+    });
+
+    articulosCarrito = [...productos];
 
 
-  articulosCarrito.push(productoObjeto);
+
+  }else{
+    articulosCarrito.push(productoObjeto);
+
+  }
 
   insertarCarritoHTML();
-  
+  guardarStorage();
+  console.log(articulosCarrito);
+
+}
+
+function guardarStorage() {
+	localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
 }
 
 
 
 function insertarCarritoHTML() {
 
-	/* Borrar contenido carrito */
-	limpiarCarrito();
+  /* Borrar contenido carrito */
+  limpiarCarrito();
 
-	/* Inserto los productos del carrito en el HTML */
-	articulosCarrito.forEach(producto => {
+  /* Inserto los productos del carrito en el HTML */
+  articulosCarrito.forEach(producto => {
 
-		/* Destructuring sobre el producto */
-		const { imagen, nombre, precio, cantidad } = producto;
+    /* Destructuring sobre el producto */
+    
+    const {
+      imagen,
+      nombre,
+      precio,
+      cantidad,
+      id
+    } = producto;
 
-		const row = document.createElement('row');
+
+
+    const row = document.createElement('div');
     row.innerHTML = `
-    <div class="row ">
+    <div class="row carta-carrito">
     <div class="col-sm-5">
-              <img class="card-img" src="img/producto1.jpg" alt="Suresh Dasari Card">
+              <img class="card-img" src="${imagen}" alt="Suresh Dasari Card">
             </div>
 
             <div class="col-sm-7">
               <div class="card-body">
-                <h5 class="card-title">Placa Madre</h5>
-                <p class="card-text">X2</p>
+                <h5 class="card-title">${nombre}</h5>
+                <p class="card-text">Precio: ${precio} Cantidad:${cantidad}</p>
                 <a href="#" class="btn btn-primary">borrar</a>
               </div>
             </div>
             </div>
 		`
 
-		contenedorCarrito.appendChild(row);
-	});
+    contenedorCarrito.appendChild(row);
+  });
 }
 
 
 function limpiarCarrito() {
-	// contenedorCarrito.innerHTML = '';
+  console.log(contenedorCarrito.firstChild)
+  // contenedorCarrito.innerHTML = '';
 
-	while (contenedorCarrito.firstChild) {
-		contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+  while (contenedorCarrito.firstChild) {
+    contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+  }
+}
+
+function limpiarProductos() {
+	while (listaProductos.firstChild) {
+		listaProductos.removeChild(listaProductos.firstChild);
 	}
+}
+
+
+
+function vaciarCarrito(){
+
+  console.log('vaciar carro');
+  articulosCarrito = [];
+  localStorage.clear();
+  insertarCarritoHTML();
+
 }
